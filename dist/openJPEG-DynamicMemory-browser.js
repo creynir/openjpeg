77abaacec84ee7768528f78c7fc480d75264314a
Joe Drago <joedrago@gmail.com>
2018-03-13 10:45:54 -0700

    Fix emccbuild to round total memory to a multiple of 16M (384M)

--- dist/openJPEG-DynamicMemory-browser.js
+++ dist/openJPEG-DynamicMemory-browser.js
@@ -1,4 +1,4 @@
-/*! OpenJPEG.js - v0.10.1 - 2018-03-12 | (c) 2016 Chris Hafey | https://github.com/chafey/openjpeg */
+/*! OpenJPEG.js - v0.10.1 - 2018-03-13 | (c) 2016 Chris Hafey | https://github.com/chafey/openjpeg */
 var OpenJPEG = function(OpenJPEG) {
   OpenJPEG = OpenJPEG || {};
 
